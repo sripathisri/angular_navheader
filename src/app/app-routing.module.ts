962724483d609f0c39902e4261d2a08f2e225b59
sripathisri <sripathisri8@gmail.com>
2022-11-23 18:15:09 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Header1Component } from './header1/header1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:Header1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
